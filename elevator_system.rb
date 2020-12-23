a4ce07f890b9fc35a1d9f6eb85e4a0f6d80c6640
Sam Holst <sawohol@gmail.com>
2020-12-22 20:33:08 -0700

    added elevator request and floor request methods with ElevatorSystem initialize method

--- elevator_system.rb
+++ elevator_system.rb
@@ -1,4 +1,18 @@
 require_relative 'elevator'
 
 class ElevatorSystem
+  attr_accessor :elevator_count, :floor_count
+
+  def initialize(elevator_count:, floor_count:)
+    @elevator_count = elevator_count
+    @floor_count = floor_count
+  end
+
+  def elevator_request(floor:, direction:)
+  end
+
+  def floor_request(floor)
+  end
 end
+
+puts ElevatorSystem.new(elevator_count: 3, floor_count: 10).inspect
